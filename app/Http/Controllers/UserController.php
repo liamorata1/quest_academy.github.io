a6ccc95780c7f59e9d4a57fc181e9217267e1ef4
Julian Miguel Avila <october12002@gmail.com>
2023-11-30 16:48:35 +0800

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function change_password()
+    {
+        $data['header_title'] = "Change Password";
+        return view('profile.change_password', $data);
+    }
 }
